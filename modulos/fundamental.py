a94549a40a585a76186d85d71dcc165210732608
gael141414 <162683083+gael141414@users.noreply.github.com>
2026-03-29 16:00:20 +0200

    Update fundamental.py

--- modulos/fundamental.py
+++ modulos/fundamental.py
@@ -10,6 +10,7 @@ from balance_analyzer import analizar_balance
 from cashflow_analyzer import analizar_flujo_efectivo
 from valuator import valorar_empresa
 from modulos.utils import obtener_valoracion_sectorial
+from charts import plot_tsr_vs_sp500
 
 def ejecutar_analisis_fundamental(ticker_input, is_df, bs_df, cf_df, res_is, res_bs, res_cf, res_val):
     earnings_yield = res_val.get('earnings_yield', 0) if res_val else 0
